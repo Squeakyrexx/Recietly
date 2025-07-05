f6976183c2a6d98648cf70da7ef5e37c3e187b1d
rowan johnson <rowanojohnson9@gmail.com>
2025-07-05 15:47:33 +0000

    please use this as the logo but you ca nchange it up a bit to make sense

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -10,21 +10,32 @@ import {
 
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path d="M4 2v20l2-1 2 1 2-1 2 1 2-1 2 1 2-1V2H4z" />
-    <path d="M8 6h8" />
-    <path d="M8 10h8" />
-    <path d="M8 14h4" />
+    <circle cx="12" cy="12" r="12" fill="#a8e6cf" />
+    <g transform="rotate(-15 12 12) translate(0.5, 0)">
+      <path
+        d="M6 20V5c0-1.1.9-2 2-2h6.5c2.49 0 4.5 2.01 4.5 4.5S16.99 12 14.5 12H19v6.5l-2-1.2-2 1.2-2-1.2-2 1.2-2-1.2-2 1.2-1-1.2V20z"
+        fill="#fff"
+        stroke="#83bcf3"
+        strokeWidth="1.2"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.5 3C12.01 3 10 5.01 10 7.5S12.01 12 14.5 12H19V7.5C19 5.01 16.99 3 14.5 3z"
+        fill="#b1d7f6"
+        stroke="#83bcf3"
+        strokeWidth="1.2"
+        strokeLinejoin="round"
+      />
+      <path d="M8 14h6m-6 2h8" stroke="#83bcf3" strokeWidth="1" strokeLinecap="round" />
+      <path d="M16 10.5 h.01 M16 9 h.01 M16 7.5 h.01" stroke="#83bcf3" strokeWidth="1.2" strokeLinecap="round" />
+    </g>
   </svg>
 );
+
 
 export const Icons = {
   Groceries: ShoppingCart,
