dba6c964c849ab84da3606dd3defd0db1cf2fb82
rowan johnson <rowanojohnson9@gmail.com>
2025-07-06 02:30:59 +0000

    this app works so well but on my phone there is some parts that look bad

--- src/components/receipts/receipt-details-dialog.tsx
+++ src/components/receipts/receipt-details-dialog.tsx
@@ -164,7 +164,7 @@ export function ReceiptDetailsDialog({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-4xl grid-rows-[auto_minmax(0,1fr)_auto] grid h-full max-h-[90vh]">
+      <DialogContent className="max-w-sm md:max-w-2xl lg:max-w-4xl grid-rows-[auto_minmax(0,1fr)_auto] grid h-full max-h-[90vh]">
         <DialogHeader>
           <DialogTitle>Receipt Details</DialogTitle>
           <DialogDescription>
